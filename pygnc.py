587b29f11310c42c3c64a820cbec1d7c08556c67
Jacob Willis <jbwillis272@gmail.com>
2023-11-08 22:50:48 -0500

    start fleshing out a task monitor

--- pygnc.py
+++ pygnc.py
@@ -2,13 +2,37 @@
 pygnc.py
 
 The file to be started by the pycubed controller.
+The only process that should communicate with the pycubed controller via stdio or tty.
 """
-import sys
+from multiprocessing import Process
 import os
+import sys
+import time
+
+import tasks.orbit_estimator as orbit_estimator
+
+
+def start_processes():
+    processes = dict()
+    orbit_estimator_p = Process(target=orbit_estimator.main, args=("~/sense.bin"))
+    orbit_estimator_p.start()
+    processes["orbit_estimator"] = orbit_estimator_p
+
+    return processes
+
+
+def monitor_processes(processes, timeout):
+    all_processes_alive = True
+    while all_processes_alive:
+        for process_name, process in processes.items():
+            if not process.is_alive():
+                all_processes_alive = False
+                print(f"{process_name} has terminated")
 
 
 def main(pygnc_timeout, spacecraft_time):
-    pass
+    processes = start_processes()
+    monitor_processes(processes, pygnc_timeout)
 
 
 if __name__ == "__main__":

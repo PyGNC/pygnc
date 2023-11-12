3b6850322055e495128c4557f1b679982243e730
Jacob Willis <jbwillis272@gmail.com>
2023-11-12 12:00:09 -0500

    GPS message

--- flight/pygnc/common/messages.py
+++ flight/pygnc/common/messages.py
@@ -76,3 +76,151 @@ class SensorMessage(MsgpackMessage):
     @property
     def sun_sensors(self):
         return np.array(self._sun_sensors)
+
+
+class GPSMessage(MsgpackMessage):
+    def __init__(
+        self,
+        spacecraft_time,
+        gps_time_status,
+        gps_week,
+        gps_milliseconds,
+        position_status,
+        position_x,
+        position_y,
+        position_z,
+        position_sig_x,
+        position_sig_y,
+        position_sig_z,
+        velocity_status,
+        velocity_x,
+        velocity_y,
+        velocity_z,
+        velocity_sig_x,
+        velocity_sig_y,
+        velocity_sig_z,
+        v_latency,
+        differential_age,
+        solution_age,
+        sats_tracked,
+        sats_in_solution,
+        msgpack_b=None,
+    ):
+        if msgpack_b is not None:
+            # initialize from msgpack data and ignore other entries
+            super()._from_msgpack_b(msgpack_b)
+        else:
+            self._spacecraft_time = spacecraft_time
+            self._gps_time_status = gps_time_status
+            self._gps_week = gps_week
+            self._gps_milliseconds = gps_milliseconds
+            self._position_status = position_status
+            self._position = (position_x, position_y, position_z)
+            self._position_sig = (position_sig_x, position_sig_y, position_sig_z)
+            self._velocity_status = velocity_status
+            self._velocity = (velocity_x, velocity_y, velocity_z)
+            self._velocity_sig = (velocity_sig_x, velocity_sig_y, velocity_sig_z)
+            self._v_latency = v_latency
+            self._differential_age = differential_age
+            self._solution_age = solution_age
+            self._sats_tracked = sats_tracked
+            self._sats_in_solution = sats_in_solution
+
+    @property
+    def as_tuple(self):
+        return (
+            self._spacecraft_time,
+            self._gps_time_status,
+            self._gps_week,
+            self._gps_milliseconds,
+            self._position_status,
+            self._position,
+            self._position_sig,
+            self._velocity_status,
+            self._velocity,
+            self._velocity_sig,
+            self._v_latency,
+            self._differential_age,
+            self._solution_age,
+            self._sats_tracked,
+            self._sats_in_solution,
+        )
+
+    def from_tuple(self, tup):
+        (
+            self._spacecraft_time,
+            self._gps_time_status,
+            self._gps_week,
+            self._gps_milliseconds,
+            self._position_status,
+            self._position,
+            self._position_sig,
+            self._velocity_status,
+            self._velocity,
+            self._velocity_sig,
+            self._v_latency,
+            self._differential_age,
+            self._solution_age,
+            self._sats_tracked,
+            self._sats_in_solution,
+        ) = tup
+
+    @property
+    def spacecraft_time(self):
+        return self._spacecraft_time
+
+    @property
+    def gps_time_status(self):
+        return self._gps_time_status
+
+    @property
+    def gps_week(self):
+        return self._gps_week
+
+    @property
+    def gps_milliseconds(self):
+        return self._gps_milliseconds
+
+    @property
+    def position_status(self):
+        return self._position_status
+
+    @property
+    def position(self):
+        return np.array(self.position)
+
+    @property
+    def position_sig(self):
+        return np.array(self._position_sig)
+
+    @property
+    def velocity_status(self):
+        return self._velocity_status
+
+    @property
+    def velocity(self):
+        return np.array(self._velocity)
+
+    @property
+    def velocity_sig(self):
+        return np.array(self._velocity_sig)
+
+    @property
+    def v_latency(self):
+        return self._v_latency
+
+    @property
+    def differential_age(self):
+        return self._differential_age
+
+    @property
+    def solution_age(self):
+        return self._solution_age
+
+    @property
+    def sats_tracked(self):
+        return self._sats_tracked
+
+    @property
+    def sats_in_solution(self):
+        return self._sats_in_solution

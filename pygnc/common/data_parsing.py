8f955b384cb62bea366668c166ada5e45c7d6602
Jacob Willis <jbwillis272@gmail.com>
2023-11-11 21:40:41 -0500

    data parsing tests passing

--- pygnc/common/data_parsing.py
+++ pygnc/common/data_parsing.py
@@ -9,8 +9,8 @@ def unpack_batch_gps_message(gps_packet):
     """
 
     spacecraft_time = int.from_bytes(gps_packet[0:4], "big")
-    gps_frame = gps_packet[4:-1]
-    ecef = struct.unpack("<dddfffBdddffffffBB", gps_frame[8:-1])
+    gps_frame = gps_packet[4:]
+    ecef = struct.unpack("<dddfffBdddffffffBB", gps_frame[8:])
     gps_data = (
         spacecraft_time,  # approx spacecraft time at gps frame (within 0.8s or less)
         gps_frame[0],  # GPS time sol status
@@ -24,6 +24,9 @@ def unpack_batch_gps_message(gps_packet):
 
 
 def unpack_batch_sensor_message(sensor_packet):
+    """
+    unpack a single sensor message that is formatted as in a batch data file.
+    """
     spacecraft_time = int.from_bytes(sensor_packet[0:4], "big")
     imu_data = struct.unpack("<hhhhhhh", sensor_packet[4:18])
     mag_measurement = np.array(imu_data[0:3]) * constants.mag_scalar_raw_to_uT
@@ -31,8 +34,30 @@ def unpack_batch_sensor_message(sensor_packet):
     gyro_measurement = np.array(imu_data[4:7]) * constants.gyro_scalar_raw_to_deg_s
     sun_raw = struct.unpack(">HHHHHH", sensor_packet[18:30])
     sun_lux = [
-        0.01 * (sun_raw_i & -61441) * (2 ^ ((sun_raw_i & 61440) >> 12))
+        0.01 * (sun_raw_i & -61441) * (2 ** ((sun_raw_i & 61440) >> 12))
         for sun_raw_i in sun_raw
     ]
 
     return (spacecraft_time, mag_measurement, raw_hall, gyro_measurement, sun_lux)
+
+
+def unpack_batch_sensor_gps_message(sensor_gps_packet):
+    # last two bytes should be \r\n
+    if not sensor_gps_packet[-2:] == b"\r\n":
+        raise ValueError(
+            "Sensor packet improperly formed, terminating `\\r\\n` missing"
+        )
+    sensor_gps_packet = sensor_gps_packet[0:-2]  # remove terminating characters
+    if not len(sensor_gps_packet) == constants.batch_packet_expected_packet_length:
+        raise ValueError("Sensor packet improperly formed, improper length")
+
+    len_sensor = constants.batch_sensor_packet_length_bytes
+    N_sensor = constants.batch_packet_num_sensor_packets
+    sensor_data_list = []
+    for i in range(N_sensor):
+        sensor_packet_i = sensor_gps_packet[i * len_sensor : (i + 1) * len_sensor]
+        sensor_data_list.append(unpack_batch_sensor_message(sensor_packet_i))
+    gps_packet = sensor_gps_packet[N_sensor * len_sensor :]
+    gps_data = unpack_batch_gps_message(gps_packet)
+
+    return sensor_data_list, gps_data

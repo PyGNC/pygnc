cd2711c973e630edfa15b7399da9ab0c4eed31fa
Jacob Willis <jbwillis272@gmail.com>
2023-11-12 12:53:22 -0500

    match naming of batch_sensor_gps_packet

--- flight/pygnc/common/constants.py
+++ flight/pygnc/common/constants.py
@@ -5,8 +5,8 @@ gyro_scalar_raw_to_deg_s = 1 / 65.6
 
 batch_sensor_packet_length_bytes = 30
 batch_gps_packet_length_bytes = 99
-batch_packet_num_sensor_packets = 5
-batch_packet_expected_packet_length = (
-    batch_packet_num_sensor_packets * batch_sensor_packet_length_bytes
+batch_sensor_gps_packet_num_sensor_packets = 5
+batch_sensor_gps_packet_expected_packet_length = (
+    batch_sensor_gps_packet_num_sensor_packets * batch_sensor_packet_length_bytes
     + batch_gps_packet_length_bytes
 )

fixed bug in jacobian

flight/pygnc/algorithms/AttitudeEstimator/mekf_core.py
@@ -4,6 +4,7 @@ import autograd.numpy as np
 #from scipy.linalg import qr
 from scipy.linalg import solve
 from scipy.linalg import block_diag
+import math
 #(TODO: implement the square root version of the MEKF to see if it improves the results?)
 
 #import a set of utils for mekf
@@ -14,6 +15,7 @@ from .mekf_utils import *
 # x[4], x[5], x[6] -> x,y,z gyro bias
 # x[7], x[8], x[9] -> x,y,z magnetometer bias
 
+#values of C are really small. may be worth implementing the square root version of the MEKF
 
 class MEKFCore:
     # constructor
@@ -30,6 +32,46 @@ class MEKFCore:
         self.x = x0
 
     #9x1 measurements vectors [6 lux measurements; 3 b vector measurents]
+    #revised get R
+    # def get_R(self, all_raw_measurements): 
+        
+    #     #positive face lux measurements
+    #     sp = all_raw_measurements[0:3][:,np.newaxis]
+
+    #     #negative face lux measurements
+    #     sn = all_raw_measurements[3:6][:,np.newaxis]
+
+    #     #magnetometer measurements
+    #     mag = all_raw_measurements[6:][:,np.newaxis]
+
+    #     #0.01 is the standard deviation from the lux measurements. Standard deviation from models.jl definition
+    #     R_lux = np.eye(6)*(0.01)**2
+
+    #     #transform 6x6 lux uncertainty to 3 dimensional vector noise
+
+    #     #jacobian for the linear covariance transformation
+    #     #each a 3x3 matrix of the partials. df/dsp and df/dsn
+    #     A_lux_1 = (np.linalg.norm(sp-sn)*np.eye(3) - (1/np.linalg.norm(sp-sn))*(sp-sn)@(sp-sn).T@(np.eye(3)))/(np.linalg.norm(sp-sn)**2)
+    #     A_lux_2 =  (np.linalg.norm(sp-sn)*(-1*np.eye(3)) - (1/np.linalg.norm(sp-sn))*(sp-sn)@(sp-sn).T@(-1*np.eye(3)))/(np.linalg.norm(sp-sn)**2)
+
+    #     A_lux = np.hstack((A_lux_1, A_lux_2))
+
+    #     R_sun_vec = A_lux@R_lux@A_lux.T
+
+    #     #do the same for the magnetometer measurement. Standard deviation from models.jl definition
+    #     R_mag = np.eye(3)*(0.6)**2
+
+    #     A_mag = (np.linalg.norm(mag)*np.eye(3) - (mag/np.linalg.norm(mag))@mag.T)/(np.linalg.norm(mag)**2)
+
+    #     R_mag_vec = A_mag@R_mag@A_mag.T
+
+    #     #measurement noise on inertial vectors
+    #     R_noise = block_diag(R_sun_vec, R_mag_vec)
+
+    #     return R_noise
+
+
+    #old get R function
     def get_R(self, all_raw_measurements): 
         
         #positive face lux measurements
@@ -49,7 +91,9 @@ class MEKFCore:
         #jacobian for the linear covariance transformation
         #each a 3x3 matrix of the partials. df/dsp and df/dsn
         A_lux_1 = (np.linalg.norm(sp-sn)*np.eye(3) - (1/np.linalg.norm(sp-sn))*(sp-sn)@(sp-sn).T)/(np.linalg.norm(sp-sn)**2)
-        A_lux_2 =  (np.linalg.norm(sp-sn)*-1*np.eye(3) - (1/np.linalg.norm(sp-sn))*(sn-sp)@(sn-sp).T)/(np.linalg.norm(sp-sn)**2)
+        #A_lux_2 =  (np.linalg.norm(sp-sn)*-1*np.eye(3) - (1/np.linalg.norm(sp-sn))*(sn-sp)@(sn-sp).T)/(np.linalg.norm(sp-sn)**2)
+
+        A_lux_2 =  (np.linalg.norm(sp-sn)*-1*np.eye(3) - (-1/np.linalg.norm(sp-sn))*(sp-sn)@(sp-sn).T)/(np.linalg.norm(sp-sn)**2)
 
         A_lux = np.hstack((A_lux_1, A_lux_2))
 
@@ -107,6 +151,8 @@ class MEKFCore:
         MEKF prediction step
         h is the timestep
         """
+        
+        #print("this is gyro measurement: ", self.u)
         # discrete dynamics function of state
         x_predicted = self.f(self.x,self.u,h)
 
@@ -133,6 +179,14 @@ class MEKFCore:
         # innovation
         #true body measurement  - predicted body measurement
         Z = body_measurement[:,np.newaxis] - predicted_body_measurement
+
+        #print("this is body measurement: ", body_measurement)
+        #print("this is predicted body: ", predicted_body_measurement)
+        
+        #print("this is Z: ", Z)
+
+        #print("this is C: ", C)
+
         
         return Z, C
     
@@ -141,7 +195,9 @@ class MEKFCore:
         MEKF Kalman Gain
         """
 
-        R = self.get_R(all_raw_measurements)
+        #R = self.get_R(all_raw_measurements)
+
+        R = block_diag(np.identity(3)*(1*math.pi/180)**2, np.identity(3)*(2*math.pi/180)**2)
 
         # innovation covariance
         S = C@P_predicted@C.T + R
@@ -171,6 +227,9 @@ class MEKFCore:
 
         #go to vector 
         delta = delta[:,0]
+
+        print("this is delta: ", delta)
+
         #get the delta quaternion from the rodrigues parameter (delta rotation)
         dq = RP_to_quaternion(delta[0:3])
         
@@ -187,7 +246,9 @@ class MEKFCore:
         self.x[7:] = x_predicted[7:] + delta[6:]
 
         #get R
-        R = self.get_R(all_raw_measurements)
+        #R = self.get_R(all_raw_measurements)
+
+        R = block_diag(np.identity(3)*(5*math.pi/180)**2, np.identity(3)*(8*math.pi/180)**2)
 
         #update the covariance
         self.P = (np.identity(9) - L_@C)@P_predicted@(np.identity(9) - L_@C).T + L_@R@L_.T

flight/pygnc/algorithms/AttitudeEstimator/orbit_mekf.py
@@ -20,7 +20,7 @@ def process_dynamics(x, u, h):
 
     mag_bias = x[7:10]
 
-    #bias gyro measurement
+    #biased gyro measurement in radians
     omega = u
 
     theta = np.linalg.norm(omega - beta)*h
@@ -49,7 +49,7 @@ class OrbitMEKF(MEKFCore):
             """
             return process_dynamics(x, u, dt)
         
-        #take in the state and an inertial measurement at that timestep. Need a way to get this
+        #take in the state and an inertial measurement at that timestep
         def measurement_function(x, inertial_measurement):
             """
             Measurement function for magnetic field and 
@@ -65,6 +65,11 @@ class OrbitMEKF(MEKFCore):
             #magnetomter bias in the body frame
             magnetometer_bias = x[7:10][:,np.newaxis]
 
+            #takes us from body to imu frame
+            #R_IMU_body = np.array([[0.0, 1.0, 0.0],
+            #          [-1.0, 0.0, 0.0],
+            #          [0.0, 0.0, -1.0]]).T
+
             #inertial measurements (not normalized)
             #b_measurement = inertial_measurement[0:3][:,np.newaxis]
             #sun_measurement = inertial_measurement[3:][:,np.newaxis]
@@ -72,6 +77,8 @@ class OrbitMEKF(MEKFCore):
             sun_measurement = inertial_measurement[0:3][:,np.newaxis]
 
             b_measurement = inertial_measurement[3:][:,np.newaxis]
+
+            #b_measurement = inertial_measurement[3:][:,np.newaxis]
 
             #this is rotation from inertial to body frame
             Q_N_B = quaternion_to_rotmatrix(q).T 
@@ -91,21 +98,41 @@ class OrbitMEKF(MEKFCore):
             #Find C -> dh/dx
             #3x3 matrices 
             #the backslash is just to start a new line in the equation
+
+            #NEW REVISED (not working. singular matrix bug)
             dy_sun_dphi = (2*np.linalg.norm(Q_N_B@sun_measurement)*hat(np.squeeze(sun_predicted)) -\
                         ((Q_N_B@sun_measurement)/np.linalg.norm(Q_N_B@sun_measurement))@sun_measurement.T\
-                        @Q_N_B.T@hat(np.squeeze(sun_predicted)))/(np.linalg.norm(Q_N_B@sun_measurement)**2)
+                        @Q_N_B.T@(2*hat(np.squeeze(sun_predicted))))/(np.linalg.norm(Q_N_B@sun_measurement)**2)
 
             #3x3 matrices
             dy_mag_dphi = (2*np.linalg.norm(Q_N_B@b_measurement + magnetometer_bias)*hat(np.squeeze(mag_predicted)) -\
                         ((Q_N_B@b_measurement + magnetometer_bias)/(np.linalg.norm(Q_N_B@b_measurement + magnetometer_bias)))\
-                        @(Q_N_B@b_measurement + magnetometer_bias).T + hat(np.squeeze(mag_predicted)))/(np.linalg.norm(Q_N_B@b_measurement + magnetometer_bias)**2)
+                        @(Q_N_B@b_measurement + magnetometer_bias).T @ (2*hat(np.squeeze(mag_predicted))))/(np.linalg.norm(Q_N_B@b_measurement + magnetometer_bias)**2)
 
             dy_mag_dmb = (np.linalg.norm(Q_N_B@b_measurement + magnetometer_bias)*np.eye(3) -\
                         ((Q_N_B@b_measurement + magnetometer_bias)/(np.linalg.norm(Q_N_B@b_measurement + magnetometer_bias)))\
                         @ (Q_N_B@b_measurement + magnetometer_bias).T @ np.eye(3))/(np.linalg.norm(Q_N_B@b_measurement + magnetometer_bias)**2)
 
+            #OLD (has bugs)
+            dy_sun_dphi_old = (2*np.linalg.norm(Q_N_B@sun_measurement)*hat(np.squeeze(sun_predicted)) -\
+                        ((Q_N_B@sun_measurement)/np.linalg.norm(Q_N_B@sun_measurement))@sun_measurement.T\
+                        @Q_N_B.T@hat(np.squeeze(sun_predicted)))/(np.linalg.norm(Q_N_B@sun_measurement)**2)
+
+            #3x3 matrices
+            # dy_mag_dphi_old = (2*np.linalg.norm(Q_N_B@b_measurement + magnetometer_bias)*hat(np.squeeze(mag_predicted)) -\
+            #             ((Q_N_B@b_measurement + magnetometer_bias)/(np.linalg.norm(Q_N_B@b_measurement + magnetometer_bias)))\
+            #             @(Q_N_B@b_measurement + magnetometer_bias).T + hat(np.squeeze(mag_predicted)))/(np.linalg.norm(Q_N_B@b_measurement + magnetometer_bias)**2)
+
+            # dy_mag_dmb_old = (np.linalg.norm(Q_N_B@b_measurement + magnetometer_bias)*np.eye(3) -\
+            #             ((Q_N_B@b_measurement + magnetometer_bias)/(np.linalg.norm(Q_N_B@b_measurement + magnetometer_bias)))\
+            #             @ (Q_N_B@b_measurement + magnetometer_bias).T @ np.eye(3))/(np.linalg.norm(Q_N_B@b_measurement + magnetometer_bias)**2)
+
+            # C_old = np.block([[dy_sun_dphi_old, np.zeros((3,6))], [dy_mag_dphi_old, np.zeros((3,3)), dy_mag_dmb_old]])
+
             C = np.block([[dy_sun_dphi, np.zeros((3,6))], [dy_mag_dphi, np.zeros((3,3)), dy_mag_dmb]])
 
+            #print("this is C: ", C)
+            #print("this is C_old: ", C_old)
             #b_measurement_biased = b_measurement + magnetometer_bias
             #inertial_measurement = np.vstack((sun_measurement, b_measurement_biased))
             #inertial_measurement  = [sun; b measurement biased]
@@ -145,10 +172,17 @@ class OrbitMEKF(MEKFCore):
         #std of gyro bias: 3 deg/s
         #std of magnetometer bias: 40 uT
         Q_noise = block_diag(np.identity(3)*(0.025*math.pi/180)**2, np.identity(3)*(3*math.pi/180)**2, np.identity(3)*(40**2))
+
+        #testing
+        #Q_noise = block_diag(np.identity(3)*(0.05*math.pi/180)**2, np.identity(3)*(5*math.pi/180)**2, np.identity(3)*(50**2))
+
+
+        #trying out new Q
+        #Q_noise = block_diag(np.identity(3)*(0.1*math.pi/180)**2, np.identity(3)*(0.1*math.pi/180)**2, np.identity(3)*(5)**2)
         #initial covariance 
-        P_0 = np.identity(9)*1e-6
+        #P_0 = np.identity(9)*1e-1
         #Initialize with Q_noise
-        #P_0 = Q_noise
+        P_0 = Q_noise
 
         #initialize the MEKF
         #gyro initialized to zero and then update based off reading the
@@ -168,7 +202,8 @@ class OrbitMEKF(MEKFCore):
         #arbitrary initial state. may need to change this
         q0 = np.array([0.18257418583505536, 0.3651483716701107, 0.5477225575051661,0.7302967433402214])
         #b0 = np.array([0.001,0.001,0.001])
-        b0 = np.array([0.05,0.05,0.05]) #this is in rad/s
+        #b0 = np.array([0.05,0.05,0.05]) #this is in rad/s
+        b0 = np.array([1,1,1])*math.pi/180  #this is in rad/s
         #mb0 = np.array([0.001,0.001,0.001])
         mb0 = np.array([40,30,25]) #this is in uT
         x0 = np.hstack((q0,b0, mb0))

flight/pygnc/tasks/attitude_estimator.py
@@ -17,8 +17,6 @@ total_lux = (1361 * 98) + 0.4 * (1361 * 98)
 
 # def get_sun_vector(sun_sensors_normalized): 
 
-
-
 #     B = np.vstack((np.eye(3), -np.eye(3)))
 
 #     #use the pseudoinverse to solve for the sun vector
@@ -35,7 +33,12 @@ total_lux = (1361 * 98) + 0.4 * (1361 * 98)
 #import the ground truth states txt file
 ground_truth_states = np.loadtxt('/home/fausto/pygnc/scenario_generator/state_history.txt', delimiter=',')
 
-print('ground truth states: ', ground_truth_states.shape)
+#print('ground truth states: ', ground_truth_states.shape)
+
+#this is from the imu frame to body
+R_body_IMU = np.array([[0.0, 1.0, 0.0],
+                    [-1.0, 0.0, 0.0],
+                    [0.0, 0.0, -1.0]])
 
 
 def get_sun_vector(sun_sensors):
@@ -63,14 +66,15 @@ def update_orbit_mekf(orbit_mekf, sensor_message, gps_message,count, prev_epoch=
 
     #print("gyro measurements deg/s: ", gyro_measurements)
 
-    #convert to rad/s
-    gyro_measurements = gyro_measurements * np.pi/180
+    #convert to rad/s and into body coordinates
+    gyro_measurements = (R_body_IMU@gyro_measurements) * np.pi/180
 
     #print("gyro measurements rad/s: ", gyro_measurements)
 
-    mag_measurements = sensor_message.mag_measurement
+    #get the magnetoemter measurements in the body frame
+    mag_measurements = R_body_IMU@sensor_message.mag_measurement
 
-    #normalize the magnetometer measurement
+    #normalize the magnetometer measurement. in the body frame. true body measurement
     mag_vector = mag_measurements/np.linalg.norm(mag_measurements)
 
     #unnormalized lux measurements
@@ -81,6 +85,7 @@ def update_orbit_mekf(orbit_mekf, sensor_message, gps_message,count, prev_epoch=
     #normalized in the function
     sun_vector = get_sun_vector(sun_measurements)
 
+    #lux measurements stacked with the magnetometer measurements
     all_raw_measurements = np.hstack((sun_measurements, mag_measurements))
 
     #print("this is sun vector: ", sun_vector)
@@ -94,17 +99,20 @@ def update_orbit_mekf(orbit_mekf, sensor_message, gps_message,count, prev_epoch=
 
     #inertial vectors #this would be from the kalman predictions as well as updates
      
-    #this is the true measurements
+    #this is the true measurements (body vectors)
     state_measurement_body = np.hstack([[sun_vector], [mag_vector]])
 
     state_measurement_body = state_measurement_body.T[:,0]
 
-    #print("state measurement body: ", state_measurement_body)
+    print("state measurement body: ", state_measurement_body)
 
     if prev_epoch is None and prev_spacecraft_time is None:
         #initialize at an arbitrary attitude
         orbit_mekf.initialize_state()
-        orbit_mekf.u = gyro_measurements
+
+        #need to convert the gyro measurements to rad/s
+        orbit_mekf.u = (R_body_IMU@gyro_measurements)*(np.pi/180)
+
         current_epoch = measurement_epoch
         current_spacecraft_time = sensor_message.spacecraft_time
 
@@ -112,18 +120,25 @@ def update_orbit_mekf(orbit_mekf, sensor_message, gps_message,count, prev_epoch=
         current_spacecraft_time = sensor_message.spacecraft_time
         dt = abs(current_spacecraft_time - prev_spacecraft_time)
 
+        #print("this is dt: ", dt)
         #avoid hard coding dt because dt can be variable...
         current_epoch = prev_epoch + dt
-
+        
+        #print("this is current epoch: ", current_epoch)
         sun_vector_inertial = brahe.sun_position(current_epoch)
         
         #50 because that is every 5 seconds in the ground truth states array. This is because that was sampled with a dt of 0.1 s
-        mag_vector_inertial = inertial_models.IGRF13(ground_truth_states[count*50,0:3], current_epoch)
+        mag_vector_inertial = inertial_models.IGRF13(ground_truth_states[count*50,0:3], current_epoch)*1e6 #converted to uT 
 
+        #print("mag vector inertial: ", mag_vector_inertial)
         #pass in not normalized. The measurement function takes care of it
         inertial_measurement = np.hstack((sun_vector_inertial, mag_vector_inertial))
 
-        orbit_mekf.u = gyro_measurements
+        #print("inertial measurement: ", inertial_measurement)
+        #need to convert the gyro measurements to rad/s and into body coordinates
+        orbit_mekf.u = (R_body_IMU@gyro_measurements)*(np.pi/180)
+
+        #print("this is gyro measurement: ", orbit_mekf.u)
         orbit_mekf.update(all_raw_measurements, state_measurement_body, inertial_measurement, dt)
 
     return current_epoch, current_spacecraft_time
@@ -175,10 +190,10 @@ def main(batch_gps_sensor_data_filepath):
 
         packet_count += 1
 
-    
     print("Batch attitude estimation completed")
 
-
+    #print size of all estimates
+    print("all estimates shape: ", all_estimates.shape)
     #print size of ground truth states
     print("ground truth states shape: ", ground_truth_states.shape)
 
@@ -186,26 +201,44 @@ def main(batch_gps_sensor_data_filepath):
     #There are 36600 measurements in the total ground truth states. The dt of the ground truth simulation 
     #is 0.1, so there is about 1 hour of data. 
 
+    #t = np.linspace(0,ground_truth_states.shape[0], num=ground_truth_states.shape[0]) 
+
+    indices = np.arange(0,ground_truth_states.shape[0], step=50)
+
     print("Final state estimate:")
     print(f"\t{orbit_mekf.x}")
     print(f"Final std dev:")
     print(f"\t{np.diag(orbit_mekf.P)}")
+
+
+    #ground truth mag bias and gyro bias
+    mag_bias_truth = np.array([15.16648, 12.85475, -36.121958])
+
+    #this is in degrees/second
+    gyro_bias_truth = np.array([1.59531306, -0.61455357, 0.30912115])
+
+    #convert to rad/s
+    gyro_bias_truth = gyro_bias_truth*np.pi/180
+
+    print("ground truth sampled size: ", ground_truth_states[indices,6:].shape)
+
+    t_size =ground_truth_states[indices,6:].shape[0]
 
     #Plotting the code for testing 
     #plot the quaternion estimates as subplots
     fig, axs = plt.subplots(4)
     fig.suptitle('Quaternion Estimates')
     axs[0].plot(all_estimates[0,:])
-    #axs[0].plot(ground_truth_states[:,6])
+    axs[0].plot(ground_truth_states[indices,6])
     axs[0].set_title('q1')
     axs[1].plot(all_estimates[1,:])
-    #axs[1].plot(ground_truth_states[:,7])
+    axs[1].plot(ground_truth_states[indices,7])
     axs[1].set_title('q2')
     axs[2].plot(all_estimates[2,:])
-    #axs[2].plot(ground_truth_states[:,8])
+    axs[2].plot(ground_truth_states[indices,8])
     axs[2].set_title('q3')
     axs[3].plot(all_estimates[3,:])
-    #axs[3].plot(ground_truth_states[:,9])
+    axs[3].plot(ground_truth_states[indices,9])
     axs[3].set_title('q4')
 
     #Get the ground truth for the gyro bias of that scenerio
@@ -213,10 +246,13 @@ def main(batch_gps_sensor_data_filepath):
     fig2, axs = plt.subplots(3)
     fig2.suptitle('Gyro Bias Estimates')
     axs[0].plot(all_estimates[4,:])
+    axs[0].plot(gyro_bias_truth[0]*np.ones(t_size))
     axs[0].set_title('x bias')
     axs[1].plot(all_estimates[5,:])
+    axs[1].plot(gyro_bias_truth[1]*np.ones(t_size))
     axs[1].set_title('y bias')
     axs[2].plot(all_estimates[6,:])
+    axs[2].plot(gyro_bias_truth[2]*np.ones(t_size))
     axs[2].set_title('z bias')
 
     #Get the ground truth for the gyro bias of that scenerio
@@ -224,15 +260,18 @@ def main(batch_gps_sensor_data_filepath):
     fig3, axs = plt.subplots(3)
     fig3.suptitle('Magnetometer Bias Estimates')
     axs[0].plot(all_estimates[7,:])
+    axs[0].plot(mag_bias_truth[0]*np.ones(t_size))
     axs[0].set_title('x bias')
     axs[1].plot(all_estimates[8,:])
+    axs[1].plot(mag_bias_truth[1]*np.ones(t_size))
     axs[1].set_title('y bias')
     axs[2].plot(all_estimates[9,:])
+    axs[2].plot(mag_bias_truth[2]*np.ones(t_size))
     axs[2].set_title('z bias')
 
 
     #show the plot
-    plt.show()
+    #plt.show()
 
     #save the plot as a png
     fig.savefig('state_estimates.png')
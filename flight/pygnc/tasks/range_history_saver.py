e6a266eb60874eed13a4ee3a47206e6932575947
Paulo Fisch <pfisch@system76-pc.localdomain>
2023-12-18 16:57:29 -0500

    works with dummy files. Need to check if number of bytes is right and file paths

--- flight/pygnc/tasks/range_history_saver.py
+++ flight/pygnc/tasks/range_history_saver.py
@@ -1,5 +1,25 @@
-from ..configuration.pygnc import batch_range_filepath
+# from ..configuration.pygnc import batch_range_filepath
 
-def main():
-    print(f"Open {batch_range_filepath}")
-    pass
+    # print(f"Open {batch_range_filepath}")
+    # read files from range filepath then read D bytes every S time steps
+def read_and_save(input_file, output_file, D, S):
+    with open(input_file, 'rb') as infile, open(output_file, 'wb') as outfile:
+        print(f"Reading {input_file} and saving every {S}th measurement to {output_file}")
+        count = 0
+        while True:
+            data = infile.read(D)
+            if not data:
+                break  # End of file
+            count += 1
+            if count % S == 0:
+                outfile.write(data)
+
+if __name__ == "__main__":
+    # Replace 'input_file.bin' and 'output_file.bin' with your actual file names
+    input_file = 'range.txt'
+    output_file = 'sparse_measurements.bin'
+
+    # Replace 100 and 5 with your desired values for D and S
+    D = 12  # Number of bytes to read and save
+    S = 30   # Save every S instances
+    read_and_save(input_file, output_file, D, S)

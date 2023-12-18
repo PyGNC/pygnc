0aff16738cc5f345ad9831e4b513a3af481a8704
Jacob Willis <jbwillis272@gmail.com>
2023-12-18 13:15:39 -0500

    black

--- flight/pygnc/configuration/tasks.py
+++ flight/pygnc/configuration/tasks.py
@@ -10,13 +10,14 @@ from ..tasks import orbit_estimator as orbit_estimator_task
 #  }
 task_list = [
     {
-        "name": "pygnc_main", # name of the task
-        "port": 5660, # port this publishes zmq messages on - should be unique
-        "main": None, #  entry function for the task (None here because this is the root task)
+        "name": "pygnc_main",  # name of the task
+        "main": None,  #  entry function for the task (None here because this is the root task)
+        "port": 5660,  # port this publishes zmq messages on - should be unique
+        "published_messages": [],
     },
     {
         "name": "orbit_estimator",
-        "port": 5661,
         "main": orbit_estimator_task.main,
+        "port": 5661,
     },
 ]

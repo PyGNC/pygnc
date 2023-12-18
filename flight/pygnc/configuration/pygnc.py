311ba231b0016dc8b47d0b4a79bded6cf5e5200c
Jacob Willis <jbwillis272@gmail.com>
2023-12-18 15:31:32 -0500

    stubbed out task

--- flight/pygnc/configuration/pygnc.py
+++ flight/pygnc/configuration/pygnc.py
@@ -2,3 +2,4 @@ import os
 import warnings
 
 batch_sensor_gps_filepath = os.path.expanduser("~/sense.bin")
+batch_range_filepath = os.path.expanduser("~/range.bin")
---
+++ flight/pygnc/tasks/range_history_saver.py
@@ -0,0 +1,5 @@
+from ..configuration.pygnc import batch_range_filepath
+
+def main():
+    print(f"Open {batch_range_filepath}")
+    pass

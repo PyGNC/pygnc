e510cccc30895eaab431d446e6c0151f56434a77
Jacob Willis <jbwillis272@gmail.com>
2023-12-18 19:30:56 -0500

    very basic telemetry task - just reading message

--- flight/pygnc/tasks/telemetry_logger.py
+++ flight/pygnc/tasks/telemetry_logger.py
@@ -1,2 +1,11 @@
+from ..common.zmq_messaging import zmqMessageSubscriber
+from ..common import messages
+
 def main():
-    pass
+    oem_sub = zmqMessageSubscriber(messages.OrbitEstimateMessage, return_latest=True)
+    while True:
+        message = oem_sub.receive(block=True)
+        print(message)
+
+if __name__ == "__main__":
+    main()
